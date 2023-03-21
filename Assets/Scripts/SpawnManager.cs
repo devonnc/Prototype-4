c39f89b3afd7dd084304bd97b0f0323a64a0ea5e
devonnc <dechavez@s.sfusd.edu>
2023-03-21 14:33:15 -0700

    finished unit 4.4 step 3

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,23 +6,35 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
 
     // Start is called before the first frame update
     void Start()
     {
-         Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+       SpawnEnemyWave(3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount === 0) {
+            SpawnEnemyWave(1);
+        }
     }
 
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for(int i = 0; i < enemiesToSpawn; i++)
+        {
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+    
+    
     private Vector3 GenerateSpawnPosition()
     {
     float spawnPosX = Random.Range(-spawnRange, spawnRange);
-    
     float spawnPosZ = Random.Range(-spawnRange, spawnRange);
     
     Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
